e1a2e860e23fed63357f7129b0fe2df4d58ffc7a
Lam Truong <nl2791999@gmail.com>
2022-07-07 15:03:16 -0500

    added chat headerInfo styles

--- frontend/src/Components/Chat.js
+++ frontend/src/Components/Chat.js
@@ -1,7 +1,32 @@
+import MoreVert from "@mui/icons-material/MoreVert";
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import { Avatar, IconButton } from "@mui/material";
 import React from "react";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+      </div>
+      <div className="chat__headerInfo">
+        <h3>Room name</h3>
+        <p>Last seen at ...</p>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
