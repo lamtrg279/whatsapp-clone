62030dfa4fbb15ddb8b23d46022c28b68da55145
Lam Truong <nl2791999@gmail.com>
2022-07-08 15:22:38 -0500

    added express & mongoose

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.get("/", (req, res) => {
+  res.status(200).send();
+});
+
+app.listen(port, () =>
+  console.log(`Server listens on localhost:${port}`)
+);
