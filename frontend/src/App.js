2c8f07729dd29f950957b5893e0e83eea8ac0b2e
Lam Truong <nl2791999@gmail.com>
2022-07-13 20:26:44 -0500

    added axios, append messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,35 @@
 import "./App.css";
 import { Sidebar, Chat } from "./Components";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
   useEffect(() => {
     const pusher = new Pusher("1bfc07828ea5674e0220", {
       cluster: "us2",
     });
 
     var channel = pusher.subscribe("messages");
-    channel.bind("inserted", (data) => {
-      alert(JSON.stringify(data));
+    channel.bind("inserted", (newMsg) => {
+      alert(JSON.stringify(newMsg));
+      setMessages([...messages, newMsg]);
     });
-  }, []);
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
 
   return (
     <div className="app">
